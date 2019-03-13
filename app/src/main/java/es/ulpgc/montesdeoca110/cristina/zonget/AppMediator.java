22e46aeda8a57aba995362b97e7152e657b9cb2c
CateRios <caterinarios@gmail.com>
2019-03-13 18:31:40 +0000

    Pequeña prueba 2

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/AppMediator.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/AppMediator.java
@@ -7,6 +7,7 @@ import es.ulpgc.montesdeoca110.cristina.zonget.SignIn.SignInState;
 public class AppMediator extends Application {
 
     //JUJUJU
+    //Funciona maldito
     //Estados de las distintas activities de la aplicación
     private SignInState signInState;
 
