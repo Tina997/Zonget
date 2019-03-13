6288d97ea7ee7e29402f5ebff69dd6eb50e52446
CateRios <caterinarios@gmail.com>
2019-03-13 20:15:31 +0000

    Terminado layout basico de la agenda del administrador

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/agendaAdmin/AgendaAdminActivity.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/agendaAdmin/AgendaAdminActivity.java
@@ -1,7 +1,9 @@
 package es.ulpgc.montesdeoca110.cristina.zonget.agendaAdmin;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -18,6 +20,15 @@ public class AgendaAdminActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda_admin);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Show the title in the action bar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(getString(R.string.agenda_activity_name));
+        }
 
         // do the setup
         AgendaAdminScreen.configure(this);
