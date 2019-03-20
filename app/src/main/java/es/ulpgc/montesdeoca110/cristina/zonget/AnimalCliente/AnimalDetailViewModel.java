24d592747500c6a9f0fa9deb344d0b4864446d69
Yguanira <47228202+Yguanira@users.noreply.github.com>
2019-03-20 12:43:42 +0000

    Delete AnimalDetailViewModel.java

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/AnimalCliente/AnimalDetailViewModel.java
+++
@@ -1,8 +0,0 @@
-package es.ulpgc.montesdeoca110.cristina.zonget.AnimalCliente;
-
-import es.ulpgc.montesdeoca110.cristina.zonget.App.AnimalClientesItem;
-
-public class AnimalDetailViewModel {
-
-    public AnimalClientesItem animal;
-}
