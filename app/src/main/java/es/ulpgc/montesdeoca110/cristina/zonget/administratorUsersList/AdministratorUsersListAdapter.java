49bd077e8afc94f4d40a1fe0368ebaa5681dca01
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2019-03-24 21:13:16 +0000

    Lista de usuarios de prueba

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/administratorUsersList/AdministratorUsersListAdapter.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/administratorUsersList/AdministratorUsersListAdapter.java
@@ -1,4 +1,66 @@
 package es.ulpgc.montesdeoca110.cristina.zonget.administratorUsersList;
 
-public class AdministratorUsersListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import es.ulpgc.montesdeoca110.cristina.zonget.R;
+import es.ulpgc.montesdeoca110.cristina.zonget.app.UserItem;
+
+public class AdministratorUsersListAdapter extends ArrayAdapter<UserItem> {
+    private final List<UserItem> itemList;
+    private final View.OnClickListener clickListener;
+
+
+    public AdministratorUsersListAdapter(Context context, List<UserItem> itemList, View.OnClickListener clickListener) {
+        super(context,0,itemList);
+        this.itemList = itemList;
+        this.clickListener = clickListener;
+    }
+
+    @Override
+    public int getCount() {
+        return itemList.size();
+    }
+
+    @Override
+    public UserItem getItem(int position) {
+        return itemList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return getItem(position).id;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View itemView = convertView;
+
+        if (itemView == null) {
+            itemView = LayoutInflater
+                    .from(parent.getContext())
+                    .inflate(R.layout.users_list_content, parent, false);
+        }
+
+        itemView.setTag(itemList.get(position));
+        itemView.setOnClickListener(clickListener);
+
+        final TextView contentView = itemView.findViewById(R.id.content);
+        contentView.setText(itemList.get(position).name);
+        contentView.setTextSize(25);
+        final TextView contentView2 = itemView.findViewById(R.id.content2);
+        contentView2.setText(itemList.get(position).DNI);
+        contentView2.setTextSize(20);
+
+
+        return itemView;
+    }
+
+
 }
--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/administratorUsersList/AdministratorUsersListModel.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/administratorUsersList/AdministratorUsersListModel.java
@@ -3,21 +3,37 @@ package es.ulpgc.montesdeoca110.cristina.zonget.administratorUsersList;
 import android.util.Log;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.FragmentActivity;
 
+import es.ulpgc.montesdeoca110.cristina.zonget.app.UserItem;
+
 public class AdministratorUsersListModel implements AdministratorUsersListContract.Model {
 
     public static String TAG = AdministratorUsersListModel.class.getSimpleName();
+    private final List<UserItem> itemList = new ArrayList<>();
+    private final int COUNT = 7;
 
     public AdministratorUsersListModel() {
-
+        for(int index = 1; index<=COUNT; index++){
+            addUser(createUser(index));
+        }
     }
 
     @Override
-    public String fetchData() {
+    public List<UserItem> fetchUsersData() {
         // Log.e(TAG, "fetchData()");
-        return "Hello";
+        return itemList;
+    }
+    private void addUser(UserItem userItem){
+        itemList.add(userItem);
+    }
+    private UserItem createUser(int index){
+        String content = "Usuario "+ index;
+        String dni = ""+ index + index + index +index + index + index + index+ index + "-X";
+        return new UserItem(index,content,dni);
     }
 }
