44588dd63506d0828ea2a0695cd12b6cbd695820
Yguanira <yguanirap@gmail.com>
2019-03-24 17:00:51 +0000

    Implementación del adapter y el userButtonMenuItem requeridos para el gridView de UserButtonsMenuList.

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/app/UserButtonMenuItem.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/app/UserButtonMenuItem.java
@@ -1,14 +1,15 @@
 package es.ulpgc.montesdeoca110.cristina.zonget.app;
 
-public class AdministratorButtonMenuItem {
+public class UserButtonMenuItem {
 
     public final int id;
     public final String button_text_label;
     public final int button_image_id;
 
-    public AdministratorButtonMenuItem(int id, String button_text_label, int button_image_id) {
+    public UserButtonMenuItem(int id, String button_text_label, int button_image_id) {
         this.id = id;
         this.button_text_label = button_text_label;
         this.button_image_id = button_image_id;
     }
+
 }
---
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/userButtonsMenuList/UserButtonsMenuListAdapter.java
@@ -0,0 +1,66 @@
+package es.ulpgc.montesdeoca110.cristina.zonget.userButtonsMenuList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import es.ulpgc.montesdeoca110.cristina.zonget.R;
+import es.ulpgc.montesdeoca110.cristina.zonget.app.UserButtonMenuItem;
+
+public class UserButtonsMenuListAdapter extends ArrayAdapter<UserButtonMenuItem> {
+
+    private final List<UserButtonMenuItem> user_buttons_list;
+
+    private final View.OnClickListener clickListener;
+
+    public UserButtonsMenuListAdapter(Context context, List<UserButtonMenuItem> items, View.OnClickListener listener) {
+
+        super(context, 0, items);
+
+        user_buttons_list = items;
+        clickListener = listener;
+    }
+
+    @Override
+    public int getCount() {
+        return user_buttons_list.size();
+    }
+
+    @Override
+    public UserButtonMenuItem getItem(int position) {
+        return user_buttons_list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return getItem(position).id;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View itemView = convertView;
+
+        if (itemView == null) {
+            itemView = LayoutInflater
+                    .from(parent.getContext())
+                    .inflate(R.layout.administrator_buttons_list_content, parent, false);
+        }
+
+        itemView.setTag(user_buttons_list.get(position));
+        itemView.setOnClickListener(clickListener);
+
+        final TextView contentView = itemView.findViewById(R.id.administrator_button_text_label);
+        final ImageView imageView = itemView.findViewById(R.id.administrator_button_image_view);
+
+        contentView.setText(user_buttons_list.get(position).button_text_label);
+        imageView.setImageResource(user_buttons_list.get(position).button_image_id);
+
+        return itemView;
+    }
+}
