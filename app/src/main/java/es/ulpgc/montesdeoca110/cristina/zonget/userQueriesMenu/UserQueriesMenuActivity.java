cefc0391a41c7da1491907b186e0ded12e27a9ee
Yguanira <yguanirap@gmail.com>
2019-03-26 15:55:06 +0000

    Ajuste de las variables de UserQueriesMenu e incorpocarion de los iconos.

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/userQueriesMenu/UserQueriesMenuActivity.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/userQueriesMenu/UserQueriesMenuActivity.java
@@ -3,8 +3,9 @@ package es.ulpgc.montesdeoca110.cristina.zonget.userQueriesMenu;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import es.ulpgc.montesdeoca110.cristina.zonget.R;
 
@@ -16,7 +17,8 @@ public class UserQueriesMenuActivity
     private UserQueriesMenuContract.Presenter presenter;
 
     //Elementos de la vista
-    private Toolbar user_queries_menu_toolbar;
+    private Toolbar toolbar;
+    private LinearLayout newQueryButton,pendingQueriesButton, finishedQueriesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +26,40 @@ public class UserQueriesMenuActivity
         setContentView(R.layout.activity_user_queries_menu);
 
         //Configuracion de la toolBar/actionBar
-        user_queries_menu_toolbar = findViewById(R.id.user_queries_menu_toolbar);
-        setSupportActionBar(user_queries_menu_toolbar);
+        toolbar = findViewById(R.id.user_queries_menu_toolbar);
+        setSupportActionBar(toolbar);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Consultas");
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //Búsqueda de los elementos de la vista
+        newQueryButton = findViewById(R.id.user_queries_menu_new_query_linear_layout);
+        pendingQueriesButton = findViewById(R.id.user_queries_menu_pending_queries_linear_layout);
+        finishedQueriesButton = findViewById(R.id.user_queries_menu_finished_queries_linear_layout);
+
+        //Listeners
+        newQueryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO llamar al presentador
+            }
+        });
+
+        pendingQueriesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO llamar al presentador
+            }
+        });
+
+        finishedQueriesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO llamar al presentador
+            }
+        });
+
 
         // do the setup
         UserQueriesMenuScreen.configure(this);
