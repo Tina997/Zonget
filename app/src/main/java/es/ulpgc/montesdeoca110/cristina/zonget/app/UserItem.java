880a8c80998573766d080b5a0aec63ebee8fa6bb
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2019-03-26 12:50:02 +0000

    Prueba

---
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/app/UserItem.java
@@ -0,0 +1,13 @@
+package es.ulpgc.montesdeoca110.cristina.zonget.app;
+
+public class UserItem {
+    public final int id;
+    public final String name, DNI;
+
+    public UserItem(int id, String name, String dni) {
+
+        this.id = id;
+        this.name = name;
+        DNI = dni;
+    }
+}
