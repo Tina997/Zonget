503cfd5eccbb2048bf2867061ed4d698fe5ebf7d
Yguanira <yguanirap@gmail.com>
2019-03-28 20:00:35 +0000

    Creación del repositorio settings en la variante complete.

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/signIn/SignInScreen.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/signIn/SignInScreen.java
@@ -4,8 +4,8 @@ import java.lang.ref.WeakReference;
 
 import android.support.v4.app.FragmentActivity;
 import es.ulpgc.montesdeoca110.cristina.zonget.app.AppMediator;
-import es.ulpgc.montesdeoca110.cristina.zonget.data.AccountsRepository;
 import es.ulpgc.montesdeoca110.cristina.zonget.data.RepositoryContract;
+import es.ulpgc.motesdeoca110.cristina.zonget.data.AccountsRepository;
 
 public class SignInScreen {
 
