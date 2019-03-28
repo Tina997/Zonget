a4277455e6a7dbce6579d4a9deb18a53ce8336d7
Yguanira <yguanirap@gmail.com>
2019-03-28 19:55:50 +0000

    Creación del repositorio accounts en la variante complete.

---
+++ app/src/mock/java/es/ulpgc/montesdeoca110/cristina/zonget/data/AccountsRepository.java
@@ -0,0 +1,36 @@
+package es.ulpgc.montesdeoca110.cristina.zonget.data;
+
+import android.content.Context;
+
+import es.ulpgc.montesdeoca110.cristina.zonget.app.AccountItem;
+
+public class AccountsRepository implements RepositoryContract.Accounts {
+
+    private static AccountsRepository INSTANCE;
+
+    private Context context;
+
+    public static AccountsRepository getInstance(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = new AccountsRepository(context);
+        }
+        return INSTANCE;
+    }
+
+    private AccountsRepository(Context context) {
+        this.context = context;
+    }
+
+    //Todo Hacer implementación correcta
+    @Override
+    public boolean checkAccountExist(String accountName, String accountPassword){
+        return true;
+    }
+
+    @Override
+    public AccountItem getAccountInfo(String accountName, String accountPassword){
+        return new AccountItem("administrator", "admin", "admin@gmail.com","admin");
+        //return new AccountItem("user", "1234", "1234@gmail.com", "1234");
+    }
+
+}
