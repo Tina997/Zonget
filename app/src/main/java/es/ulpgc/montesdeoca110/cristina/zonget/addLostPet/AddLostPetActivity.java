74997bb16a9bc6e6b0c8cf9370caffbc62d8656c
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2019-04-09 18:15:24 +0100

    Cambios menores

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/addLostPet/AddLostPetActivity.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/addLostPet/AddLostPetActivity.java
@@ -1,13 +1,18 @@
 package es.ulpgc.montesdeoca110.cristina.zonget.addLostPet;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import es.ulpgc.montesdeoca110.cristina.zonget.R;
+import es.ulpgc.montesdeoca110.cristina.zonget.administratorUsersPetDetail.AdministratorUserPetsDetailActivity;
 
 public class AddLostPetActivity
         extends AppCompatActivity implements AddLostPetContract.View {
@@ -46,11 +51,32 @@ public class AddLostPetActivity
     public void injectPresenter(AddLostPetContract.Presenter presenter) {
         this.presenter = presenter;
     }
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.administrator_appbar_buttons_add_edit_pet, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
     @Override
     public void displayData(AddLostPetViewModel viewModel) {
         //Log.e(TAG, "displayData()");
 
         // deal with the data
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_accept:
+                //presenter.insertNewPet();
+                return true;
+            default:
+                int id = item.getItemId();
+                if (id == android.R.id.home) {
+                    navigateUpTo(new Intent(this, AdministratorUserPetsDetailActivity.class));
+                    return true;
+                }
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
