8f41a251cb972bff865a5f8cf1c6b63627b87dd2
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2019-04-10 04:42:18 +0100

    Cambios menores

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/userPickDate/DialogEventAcceptDate.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/userPickDate/DialogEventAcceptDate.java
@@ -6,19 +6,23 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
 
 import es.ulpgc.montesdeoca110.cristina.zonget.R;
 
 @SuppressLint("ValidFragment")
 public class DialogEventAcceptDate extends DialogFragment {
     String day, hour;
+    private AlertDialog.Builder builder;
+
     public DialogEventAcceptDate(String date, String hours){
         hour = hours;
         day = date;
     }
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-
         AlertDialog.Builder builder =
                 new AlertDialog.Builder(getActivity(), R.style.DialogTheme);
 
