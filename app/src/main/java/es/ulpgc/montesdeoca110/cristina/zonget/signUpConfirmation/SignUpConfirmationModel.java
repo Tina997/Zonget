c917e43ac476a09125ddde9dfc831e853094f2d0
Yguanira <yguanirap@gmail.com>
2019-04-10 01:14:02 +0100

    Cambios menores.

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/signUpConfirmation/SignUpConfirmationModel.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/signUpConfirmation/SignUpConfirmationModel.java
@@ -8,6 +8,7 @@ public class SignUpConfirmationModel implements SignUpConfirmationContract.Model
     @Override
     public String getMessage(String accountName, String accountDni) {
 
+        //TODO mejorar esto
         return "Bienvido a Zonget " + accountName + "con DNI " + accountDni;
     }
 
