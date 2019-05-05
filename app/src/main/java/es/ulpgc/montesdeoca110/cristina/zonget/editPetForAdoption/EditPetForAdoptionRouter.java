c81fdb2b104de20381c65520e2b90b87b76c57bc
CateRios <caterinarios@gmail.com>
2019-05-05 20:26:31 +0100

    Realizado Checkstyle de la carpeta editPetForAdoption

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/editPetForAdoption/EditPetForAdoptionRouter.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/editPetForAdoption/EditPetForAdoptionRouter.java
@@ -1,58 +1,56 @@
 package es.ulpgc.montesdeoca110.cristina.zonget.editPetForAdoption;
 
-import android.util.Log;
-import android.content.Intent;
 import android.content.Context;
-
+import android.content.Intent;
 import es.ulpgc.montesdeoca110.cristina.zonget.app.AppMediator;
 import es.ulpgc.montesdeoca110.cristina.zonget.petsForAdoptionDetail.PetsForAdoptionDetailActivity;
 
 public class EditPetForAdoptionRouter implements EditPetForAdoptionContract.Router {
 
-    public static String TAG = EditPetForAdoptionRouter.class.getSimpleName();
+  public static String TAG = EditPetForAdoptionRouter.class.getSimpleName();
 
-    private AppMediator mediator;
+  private AppMediator mediator;
 
-    public EditPetForAdoptionRouter(AppMediator mediator) {
-        this.mediator = mediator;
-    }
+  public EditPetForAdoptionRouter(AppMediator mediator) {
+    this.mediator = mediator;
+  }
 
-    @Override
-    public void navigateToNextScreen() {
-        Context context = mediator.getApplicationContext();
-        Intent intent = new Intent(context, EditPetForAdoptionActivity.class);
-        context.startActivity(intent);
-    }
+  @Override
+  public void navigateToNextScreen() {
+    Context context = mediator.getApplicationContext();
+    Intent intent = new Intent(context, EditPetForAdoptionActivity.class);
+    context.startActivity(intent);
+  }
 
-    @Override
-    public void passDataToNextScreen(EditPetForAdoptionState state) {
-        mediator.setEditPetForAdoptionState(state);
-    }
+  @Override
+  public void passDataToNextScreen(EditPetForAdoptionState state) {
+    mediator.setEditPetForAdoptionState(state);
+  }
 
-    @Override
-    public EditPetForAdoptionState getDataFromPreviousScreen() {
-        EditPetForAdoptionState state = mediator.getEditPetForAdoptionState();
-        return state;
-    }
+  @Override
+  public EditPetForAdoptionState getDataFromPreviousScreen() {
+    EditPetForAdoptionState state = mediator.getEditPetForAdoptionState();
+    return state;
+  }
 
-    @Override
-    public String getActualThemeName() {
-        return mediator.getactualThemeName();
-    }
+  @Override
+  public String getActualThemeName() {
+    return mediator.getactualThemeName();
+  }
 
-    @Override
-    public void onBackButtonPressed() {
-        Context context = mediator.getApplicationContext();
-        Intent intent = new Intent(context, PetsForAdoptionDetailActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
-    }
+  @Override
+  public void onBackButtonPressed() {
+    Context context = mediator.getApplicationContext();
+    Intent intent = new Intent(context, PetsForAdoptionDetailActivity.class);
+    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    context.startActivity(intent);
+  }
 
-    @Override
-    public void navigateToPetForAdoptionDetailScreen(){
-        Context context = mediator.getApplicationContext();
-        Intent intent = new Intent(context, PetsForAdoptionDetailActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
-    }
+  @Override
+  public void navigateToPetForAdoptionDetailScreen() {
+    Context context = mediator.getApplicationContext();
+    Intent intent = new Intent(context, PetsForAdoptionDetailActivity.class);
+    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    context.startActivity(intent);
+  }
 }
--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/editPetForAdoption/EditPetForAdoptionViewModel.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/editPetForAdoption/EditPetForAdoptionViewModel.java
@@ -2,5 +2,5 @@ package es.ulpgc.montesdeoca110.cristina.zonget.editPetForAdoption;
 
 public class EditPetForAdoptionViewModel {
 
-    public String data;
+  public String data;
 }
