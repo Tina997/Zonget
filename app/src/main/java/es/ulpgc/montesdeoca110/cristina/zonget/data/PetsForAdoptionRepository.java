84d39e5918ab81f933202edef31cde5a2541ffb3
CateRios <caterinarios@gmail.com>
2019-05-05 20:17:55 +0100

    Realizado Checkstyle de la carpeta data

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/data/PetsForAdoptionRepository.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/data/PetsForAdoptionRepository.java
@@ -6,156 +6,150 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import es.ulpgc.montesdeoca110.cristina.zonget.app.PetForAdoptionItem;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import es.ulpgc.montesdeoca110.cristina.zonget.app.PetForAdoptionItem;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PetsForAdoptionRepository implements RepositoryContract.PetsForAdoption {
 
-    public static String TAG = PetsForAdoptionRepository.class.getSimpleName();
+  public static String TAG = PetsForAdoptionRepository.class.getSimpleName();
 
-   public static final String JSON_FILE = "petsForAdoption.json";
-   public static final String JSON_ROOT = "petsForAdoption";
+  public static final String JSON_FILE = "petsForAdoption.json";
+  public static final String JSON_ROOT = "petsForAdoption";
 
-    private static PetsForAdoptionRepository INSTANCE;
+  private static PetsForAdoptionRepository INSTANCE;
 
-    private Context context;
-    private List<PetForAdoptionItem> petsForAdoption;
+  private Context context;
+  private List<PetForAdoptionItem> petsForAdoption;
 
-    public static RepositoryContract.PetsForAdoption getInstance(Context context) {
-        if(INSTANCE == null){
-            INSTANCE = new PetsForAdoptionRepository(context);
+  public static RepositoryContract.PetsForAdoption getInstance(Context context) {
+    if (INSTANCE == null) {
+      INSTANCE = new PetsForAdoptionRepository(context);
+    }
+
+    return INSTANCE;
+  }
+
+  private PetsForAdoptionRepository(Context context) {
+    this.context = context;
+  }
+
+
+  @Override
+  public void loadCatalog(final FetchPetsForAdoptionDataCallBack callback) {
+    AsyncTask.execute(new Runnable() {
+      @Override
+      public void run() {
+        boolean error = !loadPetForAdoptionFromJSON(loadJSONFromAsset());
+
+        if (callback != null) {
+          callback.onPetsForAdoptionDataFetched(error);
+        }
+      }
+    });
+  }
+
+
+  @Override
+  public void getPetsForAdoptionList(final GetPetsForAdoptionListCallback callback) {
+    AsyncTask.execute(new Runnable() {
+
+      @Override
+      public void run() {
+        if (callback != null) {
+          callback.setPetsForAdoptionList(loadPetsForAdoptionList());
+        }
+      }
+    });
+  }
+
+
+  @Override
+  public void getPetsForAdoption(final int id, final GetPetsForAdoptionCallback callback) {
+    AsyncTask.execute(new Runnable() {
+
+      @Override
+      public void run() {
+        if (callback != null) {
+          callback.setPetsForAdoption(loadPetForAdoption(id));
+        }
+      }
+    });
+  }
+
+
+  public boolean loadPetForAdoptionFromJSON(String json) {
+    GsonBuilder gsonBuilder = new GsonBuilder();
+    Gson gson = gsonBuilder.create();
+
+    try {
+
+      JSONObject jsonObject = new JSONObject(json);
+      JSONArray jsonArray = jsonObject.getJSONArray(JSON_ROOT);
+
+      petsForAdoption = new ArrayList();
+
+      if (jsonArray.length() > 0) {
+
+        final List<PetForAdoptionItem> petsForAdoption = Arrays.asList(
+                gson.fromJson(jsonArray.toString(), PetForAdoptionItem[].class)
+        );
+
+
+        for (PetForAdoptionItem item : petsForAdoption) {
+          insertPetForAdoption(item);
         }
 
-        return INSTANCE;
+        return true;
+      }
+    } catch (JSONException error) {
+      Log.e(TAG, "error: " + error);
     }
-    private PetsForAdoptionRepository(Context context){
-        this.context = context;
-    }
+    return false;
+  }
 
 
-    @Override
-    public void loadCatalog(final FetchPetsForAdoptionDataCallBack callback) {
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-               boolean error = !loadPetForAdoptionFromJSON(loadJSONFromAsset());
+  private String loadJSONFromAsset() {
+    String json = null;
 
-               if(callback != null){
-                   callback.onPetsForAdoptionDataFetched(error);
-               }
-            }
-        });
-    }
+    try {
 
+      InputStream is = context.getAssets().open(JSON_FILE);
+      int size = is.available();
+      byte[] buffer = new byte[size];
+      is.read(buffer);
+      is.close();
+      json = new String(buffer, "UTF-8");
 
-
-
-    @Override
-    public void getPetsForAdoptionList(final GetPetsForAdoptionListCallback callback) {
-        AsyncTask.execute(new Runnable() {
-
-            @Override
-            public void run() {
-                if(callback != null) {
-                    callback.setPetsForAdoptionList(loadPetsForAdoptionList());
-                }
-            }
-        });
+    } catch (IOException error) {
+      Log.e(TAG, "error: " + error);
     }
 
+    return json;
+  }
 
 
+  private List<PetForAdoptionItem> loadPetsForAdoptionList() {
+    return petsForAdoption;
+  }
 
-    @Override
-    public void getPetsForAdoption(final int id, final GetPetsForAdoptionCallback callback) {
-        AsyncTask.execute(new Runnable() {
+  private void insertPetForAdoption(PetForAdoptionItem petForAdoption) {
+    petsForAdoption.add(petForAdoption);
+  }
 
-            @Override
-            public void run() {
-                if(callback != null) {
-                    callback.setPetsForAdoption(loadPetForAdoption(id));
-                }
-            }
-        });
+  private PetForAdoptionItem loadPetForAdoption(int id) {
+    for (PetForAdoptionItem petForAdoptionItem : petsForAdoption) {
+      if (petForAdoptionItem.id == id) {
+        return petForAdoptionItem;
+      }
     }
-
-
-
-    public boolean loadPetForAdoptionFromJSON(String json) {
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        Gson gson = gsonBuilder.create();
-
-        try {
-
-            JSONObject jsonObject = new JSONObject(json);
-            JSONArray jsonArray = jsonObject.getJSONArray(JSON_ROOT);
-
-            petsForAdoption = new ArrayList();
-
-            if (jsonArray.length() > 0) {
-
-                final List<PetForAdoptionItem> petsForAdoption = Arrays.asList(
-                        gson.fromJson(jsonArray.toString(), PetForAdoptionItem[].class)
-                );
-
-
-                for (PetForAdoptionItem item : petsForAdoption) {
-                    insertPetForAdoption(item);
-                }
-
-                return true;
-            }
-        } catch (JSONException error) {
-            Log.e(TAG, "error: " + error);
-        }
-        return false;
-    }
-
-
-
-   private String loadJSONFromAsset() {
-        String json = null;
-
-        try {
-
-            InputStream is = context.getAssets().open(JSON_FILE);
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, "UTF-8");
-
-        } catch (IOException error) {
-            Log.e(TAG, "error: " + error);
-        }
-
-        return json;
-    }
-
-
-    private List<PetForAdoptionItem> loadPetsForAdoptionList() {
-        return petsForAdoption;
-    }
-    private void insertPetForAdoption(PetForAdoptionItem petForAdoption) {
-        petsForAdoption.add(petForAdoption);
-    }
-    private PetForAdoptionItem loadPetForAdoption(int id) {
-        for (PetForAdoptionItem petForAdoptionItem: petsForAdoption){
-            if(petForAdoptionItem.id == id){
-                return petForAdoptionItem;
-            }
-        }
-        return null;
-    }
+    return null;
+  }
 
 }
--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/data/RepositoryContract.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/data/RepositoryContract.java
@@ -1,7 +1,5 @@
 package es.ulpgc.montesdeoca110.cristina.zonget.data;
 
-import java.util.List;
-
 import es.ulpgc.montesdeoca110.cristina.zonget.app.AccountItem;
 import es.ulpgc.montesdeoca110.cristina.zonget.app.AdministratorButtonMenuItem;
 import es.ulpgc.montesdeoca110.cristina.zonget.app.ChangeThemeItem;
@@ -10,151 +8,169 @@ import es.ulpgc.montesdeoca110.cristina.zonget.app.PetForAdoptionItem;
 import es.ulpgc.montesdeoca110.cristina.zonget.app.PetsItem;
 import es.ulpgc.montesdeoca110.cristina.zonget.app.UserButtonMenuItem;
 import es.ulpgc.montesdeoca110.cristina.zonget.app.UserItem;
-import es.ulpgc.montesdeoca110.cristina.zonget.app.UserPetItem;
 import es.ulpgc.motesdeoca110.cristina.zonget.data.AccountsRepository;
 import es.ulpgc.motesdeoca110.cristina.zonget.data.SettingsRepository;
 
+import java.util.List;
+
 public interface RepositoryContract {
 
-    interface Settings{
+  interface Settings {
 
-        interface FecthZongetDataCallback {
-            void onZongetDataFetched(boolean error);
-        }
-
-        void loadZonget(SettingsRepository.FecthZongetDataCallback callback);
-
-        interface GetChangeThemeListCallback {
-            void setChangeThemeList(List<ChangeThemeItem> themeList);
-        }
-
-
-        void getChangeThemeList(SettingsRepository.GetChangeThemeListCallback callback);
-
-        // ---------------- Administrator ------------------
-        interface GetAdministratorMenuButtonsListCallback {
-            void setAdministratorMenuButtonsList(List<AdministratorButtonMenuItem> administratorButtons);
-        }
-
-        void getAdministratorMenuButtonsList(SettingsRepository.GetAdministratorMenuButtonsListCallback callback);
-
-        // -------------------- User -----------------------
-        interface GetUserMenuButtonsListCallback {
-            void setUserMenuButtonsList(List<UserButtonMenuItem> userButtons);
-        }
-
-        void getUserMenuButtonsList(SettingsRepository.GetUserMenuButtonsListCallback callback);
-
+    interface FecthZongetDataCallback {
+      void onZongetDataFetched(boolean error);
     }
 
-    interface Accounts {
+    void loadZonget(SettingsRepository.FecthZongetDataCallback callback);
 
-        interface FecthZongetDataCallback {
-            void onZongetDataFetched(boolean error);
-        }
-
-        void loadZonget(boolean clearFirst, AccountsRepository.FecthZongetDataCallback callback);
-
-        interface GetCheckAccountExistCallback {
-            void setCheckAccountExist(boolean exist, AccountItem account);
-        }
-
-        void getCheckAccount(String accountName, String accountPassword, RepositoryContract.Accounts.GetCheckAccountExistCallback callback);
-
-        interface CheckNewAccountDataExistCallback {
-            void setNewAccountExistCallBack(boolean exist, int lastId);
-        }
-
-        void checkNewAccountDataExist(String dni, String email, RepositoryContract.Accounts.CheckNewAccountDataExistCallback callback);
-
-        interface InsertNewAccountCallback{
-            void onNewAccountInserted();
-        }
-
-        void insertNewAccount(AccountItem account, RepositoryContract.Accounts.InsertNewAccountCallback callback);
-
-
-        interface GetUserPetsListCallback {
-            void setUserPetsList(List<PetsItem> userPets);
-        }
-
-        interface GetUserPetsCallback {
-            void setUserPets(PetsItem user);
-        }
-
-        //void loadUserPets(boolean clearFirst, AccountsRepository.FetchUserPetsDataCallBack callback);
-        void getUserPetsList(AccountItem accountItem, AccountsRepository.GetUserPetsListCallback callback);
-        void getUserPetsList(int userId, AccountsRepository.GetUserPetsListCallback callback);
-
-    }
-    interface LostPets{
-
-        interface FetchLostPetsDataCallBack{
-            void onLostPetsDataFetched(boolean error);
-        }
-        interface GetLostPetsListCallback{
-            void setLostPetsList(List<LostPetItem> lostPets);
-        }
-        interface GetLostPetsCallback{
-            void setLostPets(LostPetItem lostPet);
-        }
-        void loadLostPets(boolean clearFirst, LostPetsRepository.FetchLostPetsDataCallBack callback);
-        void getLostPetsList(LostPetsRepository.GetLostPetsListCallback callback);
-
-        void getLostPets(int id, LostPetsRepository.GetLostPetsCallback callback);
+    interface GetChangeThemeListCallback {
+      void setChangeThemeList(List<ChangeThemeItem> themeList);
     }
 
-    interface PetsForAdoption{
 
-        interface FetchPetsForAdoptionDataCallBack{
-            void onPetsForAdoptionDataFetched(boolean error);
-        }
-        interface GetPetsForAdoptionListCallback{
-            void setPetsForAdoptionList(List<PetForAdoptionItem> items);
-        }
-        interface GetPetsForAdoptionCallback{
-            void setPetsForAdoption(PetForAdoptionItem petForAdoption);
-        }
-        void loadCatalog(PetsForAdoptionRepository.FetchPetsForAdoptionDataCallBack callback);
-        void getPetsForAdoptionList(PetsForAdoptionRepository.GetPetsForAdoptionListCallback callback);
+    void getChangeThemeList(SettingsRepository.GetChangeThemeListCallback callback);
 
-        void getPetsForAdoption(int id, PetsForAdoptionRepository.GetPetsForAdoptionCallback callback);
+    // ---------------- Administrator ------------------
+    interface GetAdministratorMenuButtonsListCallback {
+      void setAdministratorMenuButtonsList(List<AdministratorButtonMenuItem> administratorButtons);
     }
-    interface Users {
 
-        interface FetchUsersDataCallBack {
-            void onUsersDataFetched(boolean error);
-        }
+    void getAdministratorMenuButtonsList(
+            SettingsRepository.GetAdministratorMenuButtonsListCallback callback);
 
-        interface GetUsersListCallback {
-            void setUsersList(List<UserItem> users);
-        }
-
-        interface GetUsersCallback {
-            void setUsers(UserItem user);
-        }
-
-        void loadUsers(boolean clearFirst, UsersRepository.FetchUsersDataCallBack callback);
-
-        void getUsersList(UsersRepository.GetUsersListCallback callback);
+    // -------------------- User -----------------------
+    interface GetUserMenuButtonsListCallback {
+      void setUserMenuButtonsList(List<UserButtonMenuItem> userButtons);
     }
-   /* interface UserPets {
 
-        interface FetchUserPetsDataCallBack {
-            void onUserPetsDataFetched(boolean error);
-        }
+    void getUserMenuButtonsList(SettingsRepository.GetUserMenuButtonsListCallback callback);
 
-        interface GetUserPetsListCallback {
-            void setUserPetsList(List<PetsItem> userPets);
-        }
+  }
 
-        interface GetUserPetsCallback {
-            void setUserPets(PetsItem user);
-        }
+  interface Accounts {
 
-        void loadUserPets(boolean clearFirst, UserPetsRepository.FetchUserPetsDataCallBack callback);
-
-        void getUserPetsList(UserPetsRepository.GetUserPetsListCallback callback);
-    }*/
-
+    interface FecthZongetDataCallback {
+      void onZongetDataFetched(boolean error);
     }
+
+    void loadZonget(boolean clearFirst, AccountsRepository.FecthZongetDataCallback callback);
+
+    interface GetCheckAccountExistCallback {
+      void setCheckAccountExist(boolean exist, AccountItem account);
+    }
+
+    void getCheckAccount(String accountName, String accountPassword,
+                         RepositoryContract.Accounts.GetCheckAccountExistCallback callback);
+
+    interface CheckNewAccountDataExistCallback {
+      void setNewAccountExistCallBack(boolean exist, int lastId);
+    }
+
+    void checkNewAccountDataExist(String dni, String email, RepositoryContract.Accounts
+            .CheckNewAccountDataExistCallback callback);
+
+    interface InsertNewAccountCallback {
+      void onNewAccountInserted();
+    }
+
+    void insertNewAccount(AccountItem account,
+                          RepositoryContract.Accounts.InsertNewAccountCallback callback);
+
+
+    interface GetUserPetsListCallback {
+      void setUserPetsList(List<PetsItem> userPets);
+    }
+
+    interface GetUserPetsCallback {
+      void setUserPets(PetsItem user);
+    }
+
+    //void loadUserPets(boolean clearFirst, AccountsRepository.FetchUserPetsDataCallBack callback);
+    void getUserPetsList(AccountItem accountItem,
+                         AccountsRepository.GetUserPetsListCallback callback);
+
+    void getUserPetsList(int userId, AccountsRepository.GetUserPetsListCallback callback);
+
+  }
+
+  interface LostPets {
+
+    interface FetchLostPetsDataCallBack {
+      void onLostPetsDataFetched(boolean error);
+    }
+
+    interface GetLostPetsListCallback {
+      void setLostPetsList(List<LostPetItem> lostPets);
+    }
+
+    interface GetLostPetsCallback {
+      void setLostPets(LostPetItem lostPet);
+    }
+
+    void loadLostPets(boolean clearFirst, LostPetsRepository.FetchLostPetsDataCallBack callback);
+
+    void getLostPetsList(LostPetsRepository.GetLostPetsListCallback callback);
+
+    void getLostPets(int id, LostPetsRepository.GetLostPetsCallback callback);
+  }
+
+  interface PetsForAdoption {
+
+    interface FetchPetsForAdoptionDataCallBack {
+      void onPetsForAdoptionDataFetched(boolean error);
+    }
+
+    interface GetPetsForAdoptionListCallback {
+      void setPetsForAdoptionList(List<PetForAdoptionItem> items);
+    }
+
+    interface GetPetsForAdoptionCallback {
+      void setPetsForAdoption(PetForAdoptionItem petForAdoption);
+    }
+
+    void loadCatalog(PetsForAdoptionRepository.FetchPetsForAdoptionDataCallBack callback);
+
+    void getPetsForAdoptionList(PetsForAdoptionRepository.GetPetsForAdoptionListCallback callback);
+
+    void getPetsForAdoption(int id, PetsForAdoptionRepository.GetPetsForAdoptionCallback callback);
+  }
+
+  interface Users {
+
+    interface FetchUsersDataCallBack {
+      void onUsersDataFetched(boolean error);
+    }
+
+    interface GetUsersListCallback {
+      void setUsersList(List<UserItem> users);
+    }
+
+    interface GetUsersCallback {
+      void setUsers(UserItem user);
+    }
+
+    void loadUsers(boolean clearFirst, UsersRepository.FetchUsersDataCallBack callback);
+
+    void getUsersList(UsersRepository.GetUsersListCallback callback);
+  }
+  /* interface UserPets {
+
+      interface FetchUserPetsDataCallBack {
+          void onUserPetsDataFetched(boolean error);
+      }
+
+      interface GetUserPetsListCallback {
+          void setUserPetsList(List<PetsItem> userPets);
+      }
+
+      interface GetUserPetsCallback {
+          void setUserPets(PetsItem user);
+      }
+
+      void loadUserPets(boolean clearFirst,
+      UserPetsRepository.FetchUserPetsDataCallBack callback);
+
+      void getUserPetsList(UserPetsRepository.GetUserPetsListCallback callback);
+  }*/
+
+}
