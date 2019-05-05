c8aae5abc5d39d1ae1bb2759fd8bd9e9540d7df8
CateRios <caterinarios@gmail.com>
2019-05-05 20:24:58 +0100

    Realizado Checkstyle de SplashActivity

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/SplashActivity.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/SplashActivity.java
@@ -1,51 +1,49 @@
 package es.ulpgc.montesdeoca110.cristina.zonget;
 
-import android.os.Bundle;
-import android.os.Handler;
 import android.content.Intent;
+import android.os.Bundle;
+
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import es.ulpgc.montesdeoca110.cristina.zonget.signIn.SignInActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private Handler mWaitHandler = new Handler();
+  private Handler mWaitHandler = new Handler();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        //Mensaje de Cris: Cambie el tema y ahora no te hace falta esto por eso te lo comente
-        // Oculta la barra del título
-        /*requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
-        */
-        setContentView(R.layout.activity_splash);
+    setContentView(R.layout.activity_splash);
 
-        mWaitHandler.postDelayed(new Runnable() {
+    mWaitHandler.postDelayed(new Runnable() {
 
-            @Override
-            public void run() {
-                try {
-                    //Siguiente activity
-                    Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
-                    startActivity(intent);
+      @Override
+      public void run() {
+        try {
+          //Siguiente activity
+          Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
+          startActivity(intent);
 
-                    //Let's Finish Splash Activity since we don't want to show this when user press back button.
-                    finish();
-                } catch (Exception ignored) {
-                    ignored.printStackTrace();
-                }
-            }
-        }, 3000);
-    }
+          //Let's Finish Splash Activity since we don't want to
+          // show this when user press back button.
+          finish();
+        } catch (Exception ignored) {
+          ignored.printStackTrace();
+        }
+      }
+    }, 3000);
+  }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
 
-        //Remove all the callbacks otherwise navigation will execute even after activity is killed or closed.
-        mWaitHandler.removeCallbacksAndMessages(null);
-    }
+    //Remove all the callbacks otherwise navigation will execute
+    // even after activity is killed or closed.
+    mWaitHandler.removeCallbacksAndMessages(null);
+  }
 
 }
