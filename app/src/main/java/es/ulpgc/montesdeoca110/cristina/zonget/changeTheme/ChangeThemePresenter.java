74dddc7d6399b1d5ec6a1ac1f9d8f9033de25da8
CateRios <caterinarios@gmail.com>
2019-05-05 20:13:17 +0100

    Realizado Checkstyle de la carpeta ChangeTheme

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/changeTheme/ChangeThemePresenter.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/changeTheme/ChangeThemePresenter.java
@@ -1,66 +1,66 @@
 package es.ulpgc.montesdeoca110.cristina.zonget.changeTheme;
 
-import java.lang.ref.WeakReference;
-import java.util.List;
-
 import es.ulpgc.montesdeoca110.cristina.zonget.app.ChangeThemeItem;
 import es.ulpgc.montesdeoca110.cristina.zonget.app.ChangeThemeToMenuState;
 import es.ulpgc.montesdeoca110.cristina.zonget.data.RepositoryContract;
+import java.lang.ref.WeakReference;
+import java.util.List;
 
 public class ChangeThemePresenter implements ChangeThemeContract.Presenter {
 
-    private WeakReference<ChangeThemeContract.View> view;
-    private ChangeThemeViewModel viewModel;
-    private ChangeThemeContract.Model model;
-    private ChangeThemeContract.Router router;
+  private WeakReference<ChangeThemeContract.View> view;
+  private ChangeThemeViewModel viewModel;
+  private ChangeThemeContract.Model model;
+  private ChangeThemeContract.Router router;
 
-    public ChangeThemePresenter(ChangeThemeState state) {
-        viewModel = state;
-    }
+  public ChangeThemePresenter(ChangeThemeState state) {
+    viewModel = state;
+  }
 
-    @Override
-    public String getActualThemeName() {
-        return router.getActualThemeName();
-    }
+  @Override
+  public String getActualThemeName() {
+    return router.getActualThemeName();
+  }
 
-    @Override
-    public void injectView(WeakReference<ChangeThemeContract.View> view) {
-        this.view = view;
-    }
+  @Override
+  public void injectView(WeakReference<ChangeThemeContract.View> view) {
+    this.view = view;
+  }
 
-    @Override
-    public void injectModel(ChangeThemeContract.Model model) {
-        this.model = model;
-    }
+  @Override
+  public void injectModel(ChangeThemeContract.Model model) {
+    this.model = model;
+  }
 
-    @Override
-    public void injectRouter(ChangeThemeContract.Router router) {
-        this.router = router;
-    }
+  @Override
+  public void injectRouter(ChangeThemeContract.Router router) {
+    this.router = router;
+  }
 
-    @Override
-    public void backButtonPressed() {
-        ChangeThemeToMenuState state = new ChangeThemeToMenuState();
-        state.themeChanged = true;
-        view.get().finish();
-        router.passDataToMenuScreen(state);
-        router.navigateToMenuScreen();
-    }
+  @Override
+  public void backButtonPressed() {
+    ChangeThemeToMenuState state = new ChangeThemeToMenuState();
+    state.themeChanged = true;
+    view.get().finish();
+    router.passDataToMenuScreen(state);
+    router.navigateToMenuScreen();
+  }
 
-    @Override
-    public void fetchChangeThemeListData() {
-        model.fetchChangeThemeListData(new RepositoryContract.Settings.GetChangeThemeListCallback() {
-            @Override
-            public void setChangeThemeList(List<ChangeThemeItem> themeList) {
-                viewModel.themeList = themeList;
-                view.get().displayChangeThemeListData(viewModel);
-            }
-        });}
+  @Override
+  public void fetchChangeThemeListData() {
+    model.fetchChangeThemeListData(new RepositoryContract.Settings.GetChangeThemeListCallback() {
+      @Override
+      public void setChangeThemeList(List<ChangeThemeItem> themeList) {
+        viewModel.themeList = themeList;
+        view.get().displayChangeThemeListData(viewModel);
+      }
+    });
+  }
 
-    @Override
-    public void selectChangeThemeListData(ChangeThemeItem item) {
-        router.changeActualTheme(item.themeName);
-        viewModel.themeChanged = item.themeName;
-        view.get().reboot();
-    }
+  @Override
+  public void selectChangeThemeListData(ChangeThemeItem item) {
+    router.changeActualTheme(item.themeName);
+    viewModel.themeChanged = item.themeName;
+    view.get().reboot();
+  }
 }
--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/changeTheme/ChangeThemeViewModel.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/changeTheme/ChangeThemeViewModel.java
@@ -1,12 +1,11 @@
 package es.ulpgc.montesdeoca110.cristina.zonget.changeTheme;
 
-import java.util.List;
-
 import es.ulpgc.montesdeoca110.cristina.zonget.app.ChangeThemeItem;
+import java.util.List;
 
 public class ChangeThemeViewModel {
 
-    public String themeChanged;
+  public String themeChanged;
 
-    public List<ChangeThemeItem> themeList;
+  public List<ChangeThemeItem> themeList;
 }
