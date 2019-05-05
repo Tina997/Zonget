c1e1f00ced803147e9ddb5c32ccd9711e9de78e0
CateRios <caterinarios@gmail.com>
2019-05-05 21:15:07 +0100

    Realizado Checkstyle de la carpeta usersQueriesMenu

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/userQueriesMenu/UserQueriesMenuPresenter.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/userQueriesMenu/UserQueriesMenuPresenter.java
@@ -4,57 +4,57 @@ import java.lang.ref.WeakReference;
 
 public class UserQueriesMenuPresenter implements UserQueriesMenuContract.Presenter {
 
-    private WeakReference<UserQueriesMenuContract.View> view;
-    private UserQueriesMenuViewModel viewModel;
-    private UserQueriesMenuContract.Model model;
-    private UserQueriesMenuContract.Router router;
+  private WeakReference<UserQueriesMenuContract.View> view;
+  private UserQueriesMenuViewModel viewModel;
+  private UserQueriesMenuContract.Model model;
+  private UserQueriesMenuContract.Router router;
 
-    public UserQueriesMenuPresenter(UserQueriesMenuState state) {
-        viewModel = state;
-    }
+  public UserQueriesMenuPresenter(UserQueriesMenuState state) {
+    viewModel = state;
+  }
 
-    @Override
-    public void injectView(WeakReference<UserQueriesMenuContract.View> view) {
-        this.view = view;
-    }
+  @Override
+  public void injectView(WeakReference<UserQueriesMenuContract.View> view) {
+    this.view = view;
+  }
 
-    @Override
-    public void injectModel(UserQueriesMenuContract.Model model) {
-        this.model = model;
-    }
+  @Override
+  public void injectModel(UserQueriesMenuContract.Model model) {
+    this.model = model;
+  }
 
-    @Override
-    public void injectRouter(UserQueriesMenuContract.Router router) {
-        this.router = router;
-    }
+  @Override
+  public void injectRouter(UserQueriesMenuContract.Router router) {
+    this.router = router;
+  }
 
-    @Override
-    public void backButtonPressed() {
-        router.navigateToUserMenuScreen();
-    }
+  @Override
+  public void backButtonPressed() {
+    router.navigateToUserMenuScreen();
+  }
 
-    @Override
-    public void newQueryButtonPressed() {
-        router.passDataToUserNewQueryScreen();
-        router.navigateToUserNewQueryScreen();
-    }
+  @Override
+  public void newQueryButtonPressed() {
+    router.passDataToUserNewQueryScreen();
+    router.navigateToUserNewQueryScreen();
+  }
 
-    @Override
-    public void pendingQueriesButtonPressed() {
-        router.passDataToUserPendingQueriesListScreen();
-        router.navigateToUserPendingQueriesScreen();
-    }
+  @Override
+  public void pendingQueriesButtonPressed() {
+    router.passDataToUserPendingQueriesListScreen();
+    router.navigateToUserPendingQueriesScreen();
+  }
 
-    @Override
-    public void finishedQueriesButtonPressed() {
-        router.passDataToUserFinishedQueriesListScreen();
-        router.navigateToUserFinishedQueriesScreen();
-    }
+  @Override
+  public void finishedQueriesButtonPressed() {
+    router.passDataToUserFinishedQueriesListScreen();
+    router.navigateToUserFinishedQueriesScreen();
+  }
 
-    @Override
-    public void fetchUserQueriesMenuData() {
+  @Override
+  public void fetchUserQueriesMenuData() {
 
-    }
+  }
 
 
 }
