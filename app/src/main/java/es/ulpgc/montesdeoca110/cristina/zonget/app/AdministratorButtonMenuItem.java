54abead8ce127b3db7b0e1a666e123d053672dd7
Yguanira <yguanirap@gmail.com>
2019-05-07 22:11:23 +0100

    Test del menú del administrador.

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/app/AdministratorButtonMenuItem.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/app/AdministratorButtonMenuItem.java
@@ -5,7 +5,7 @@ public class AdministratorButtonMenuItem {
     public final int id;
     public final String text;
     public final String image;
-    public final String activity;
+    public String activity;
 
     public AdministratorButtonMenuItem(int id, String text, String image) {
         this.id = id;
