3d46ed5c401d79291f049c664ec6981c239d191d
Yguanira <yguanirap@gmail.com>
2019-06-01 21:57:09 +0100

    Comentarios de PetsForAdoptionRepository.

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/data/PetsForAdoptionRepository.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/data/PetsForAdoptionRepository.java
@@ -1,5 +1,6 @@
 package es.ulpgc.montesdeoca110.cristina.zonget.data;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
@@ -13,8 +14,8 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import es.ulpgc.montesdeoca110.cristina.zonget.app.PetForAdoptionItem;
@@ -23,12 +24,16 @@ public class PetsForAdoptionRepository implements RepositoryContract.PetsForAdop
 
     public static String TAG = PetsForAdoptionRepository.class.getSimpleName();
 
-    public static final String JSON_FILE = "petsForAdoption.json";
-    public static final String JSON_ROOT = "petsForAdoption";
+    // ============================= Variables globales ===========================================
 
+    @SuppressLint("StaticFieldLeak")
     private static PetsForAdoptionRepository INSTANCE;
-
     private Context context;
+
+    private static final String JSON_FILE = "petsForAdoption.json";
+    private static final String JSON_ROOT = "petsForAdoption";
+
+    // --- Lista
     private List<PetForAdoptionItem> petsForAdoption;
 
     public static RepositoryContract.PetsForAdoption getInstance(Context context) {
@@ -42,6 +47,7 @@ public class PetsForAdoptionRepository implements RepositoryContract.PetsForAdop
         this.context = context;
     }
 
+    // ==================================== Métodos ===============================================
 
     @Override
     public void loadCatalog(final FetchPetsForAdoptionDataCallBack callback) {
@@ -57,9 +63,6 @@ public class PetsForAdoptionRepository implements RepositoryContract.PetsForAdop
         });
     }
 
-
-
-
     @Override
     public void getPetsForAdoptionList(final GetPetsForAdoptionListCallback callback) {
         AsyncTask.execute(new Runnable() {
@@ -72,9 +75,6 @@ public class PetsForAdoptionRepository implements RepositoryContract.PetsForAdop
             }
         });
     }
-
-
-
 
     @Override
     public void getPetForAdoption(final int id, final GetPetForAdoptionCallback callback) {
@@ -89,24 +89,27 @@ public class PetsForAdoptionRepository implements RepositoryContract.PetsForAdop
         });
     }
 
+    // ================================= Métodos privados =========================================
 
-
-    public boolean loadPetForAdoptionFromJSON(String json) {
+    /** Este método divide la información que contiene el json y pasa la que indicamos a la
+     * variable del repositorio.
+     *
+     * @param json Archivo JSON que contiene la información de la aplicación.
+     * @return boolean que nos indica si se ha cargado la información correctamente o no.
+     */
+    private boolean loadPetForAdoptionFromJSON(String json) {
         GsonBuilder gsonBuilder = new GsonBuilder();
         Gson gson = gsonBuilder.create();
 
         try {
-
             JSONObject jsonObject = new JSONObject(json);
             JSONArray jsonArray = jsonObject.getJSONArray(JSON_ROOT);
 
-            petsForAdoption = new ArrayList();
+            petsForAdoption = new ArrayList<>();
 
             if (jsonArray.length() > 0) {
 
-                final List<PetForAdoptionItem> petsForAdoption = Arrays.asList(
-                        gson.fromJson(jsonArray.toString(), PetForAdoptionItem[].class)
-                );
+                final PetForAdoptionItem[] petsForAdoption = gson.fromJson(jsonArray.toString(), PetForAdoptionItem[].class);
 
 
                 for (PetForAdoptionItem item : petsForAdoption) {
@@ -121,34 +124,49 @@ public class PetsForAdoptionRepository implements RepositoryContract.PetsForAdop
         return false;
     }
 
-
-
+    /** Este método carga desde la carpeta Assets del proyecto el json con la información de la
+     * aplicación.
+     *
+     * @return La información que contiene el json.
+     */
     private String loadJSONFromAsset() {
         String json = null;
 
         try {
-
             InputStream is = context.getAssets().open(JSON_FILE);
             int size = is.available();
             byte[] buffer = new byte[size];
             is.read(buffer);
             is.close();
-            json = new String(buffer, "UTF-8");
+            json = new String(buffer, StandardCharsets.UTF_8);
 
         } catch (IOException error) {
             Log.e(TAG, "error: " + error);
         }
-
         return json;
     }
 
-
+    /** Este método obtiene la lista de mascotas para adoptar.
+     *
+     * @return Lista de mascotas para adoptar.
+     */
     private List<PetForAdoptionItem> loadPetsForAdoptionList() {
         return petsForAdoption;
     }
+
+    /** Este método inserta una mascota para adoptar.
+     *
+     * @param petForAdoption Animal para adoptar.
+     */
     private void insertPetForAdoption(PetForAdoptionItem petForAdoption) {
         petsForAdoption.add(petForAdoption);
     }
+
+    /** Este método obtiene una mascota para adoptar a partir de su identificador.
+     *
+     * @param id Identificador de la mascota para adoptar.
+     * @return Mascota para adoptar.
+     */
     private PetForAdoptionItem loadPetForAdoption(int id) {
         for (PetForAdoptionItem petForAdoptionItem: petsForAdoption){
             if(petForAdoptionItem.id == id){
