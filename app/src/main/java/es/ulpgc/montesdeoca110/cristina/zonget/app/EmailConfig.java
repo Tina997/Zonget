dd78906e1c6d9899b3f4bac9ab242c19885f7670
Yguanira <47228202+Yguanira@users.noreply.github.com>
2019-10-22 20:35:45 +0100

    Update EmailConfig.java

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/app/EmailConfig.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/zonget/app/EmailConfig.java
@@ -3,7 +3,7 @@ package es.ulpgc.montesdeoca110.cristina.zonget.app;
 class EmailConfig {
 
   // default Zonget email data
-  public static final String ADMIN_EMAIL = "yguanirap@gmail.com";
-  public static final String ADMIN_PASSWORD = "Narutoshippuden97";
+  public static final String ADMIN_EMAIL = "xxxxxxxxx@gmail.com";
+  public static final String ADMIN_PASSWORD = "00000000000000000";
 
 }
